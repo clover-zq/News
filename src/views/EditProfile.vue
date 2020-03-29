f378540194153006f7db64f32241f018f5f37a5b
clover-zq <60808831+clover-zq@users.noreply.github.com>
2020-03-29 23:34:37 +0800

    加载编辑页面时，回显

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -1,18 +1,71 @@
 <template>
-  <!-- 头部导航条 -->
-  <NavigateBar title="编辑资料"></NavigateBar>
+  <div class="container">
+    <!-- 头部导航条 -->
+    <NavigateBar title="编辑资料"></NavigateBar>
+    <div class="avater">
+      <img :src="$axios.defaults.baseURL + userInfo.head_img" />
+    </div>
+
+    <!--列表 -->
+    <Listbar label="昵称" :tips="userInfo.nickname" />
+    <Listbar label="密码" tips="******" />
+    <Listbar label="性别" :tips="['女','男'][ userInfo.gender ]" />
+  </div>
 </template>
 
 <script>
 //引入组件
 import NavigateBar from "@/components/NavigateBar";
+import Listbar from "@/components/Listbar";
+
 //注册组件
 export default {
   components: {
-    NavigateBar
+    NavigateBar,
+    Listbar
+  },
+  //准备数据
+  data() {
+    return {
+      userInfo: {}
+    };
+  },
+
+  mounted() {
+    //请求用户详情信息
+    //从本地获取到token和id
+    const jsonStr = localStorage.getItem("userInfo");
+    const userJson = JSON.parse(jsonStr);
+
+    // 发异步请求
+    this.$axios({
+      url: "/user/" + userJson.user.id,
+      //设置一个请求头
+      headers: {
+        Authorization: userJson.token
+      }
+    }).then(res => {
+      const { data } = res.data;
+      this.userInfo = data;
+    });
   }
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  .avater {
+    width: 70/360 * 100vw;
+    height: 70/360 * 100vw;
+    margin: 20/360 * 100vw auto 26/360 * 100vw;
+    img {
+      display: block;
+      width: 70/360 * 100vw;
+      height: 70/360 * 100vw;
+      border-radius: 50%;
+      //   margin: 20/360 * 100vw 0;
+      object-fit: cover;
+    }
+  }
+}
 </style>
